044018db07a02ef5842c3c74f2f35039e2e57263
Satish Venkatakrishnan <satish860@gmail.com>
2023-01-21 17:20:55 +0530

    Removed Warning

--- src/ShareSafe.API/Files/CreateFiles/CreateFile.cs
+++ src/ShareSafe.API/Files/CreateFiles/CreateFile.cs
@@ -4,14 +4,14 @@ namespace ShareSafe.API.Files.CreateFiles
 {
     public class CreateFile
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         public ObjectId Id { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public string OwnedBy { get; set; }
+        public string? OwnedBy { get; set; }
 
-        public string Signature { get; set; }
+        public string? Signature { get; set; }
     }
 }
