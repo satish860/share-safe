18fb532b94baee7993ac8eecc40510b4980fadab
Satish Venkatakrishnan <satish860@gmail.com>
2023-01-22 11:22:37 +0530

    Delete Endpoint done with details

---
+++ src/ShareSafe.API/Files/DeleteFile/DeleteFileEndpoint.cs
@@ -0,0 +1,41 @@
+﻿using FastEndpoints;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ShareSafe.API.Files.DeleteFile
+{
+    public class DeleteFileEndpoint:EndpointWithoutRequest
+    {
+        private readonly IMongoClient mongoClient;
+        private readonly IConfiguration configuration;
+
+        public DeleteFileEndpoint(IMongoClient mongoClient,IConfiguration configuration)
+        {
+            this.mongoClient = mongoClient;
+            this.configuration = configuration;
+        }
+
+        public override void Configure()
+        {
+            Delete("/files/{fileid}");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            var fileId = Route<ObjectId>("fileid");
+            var DBName = this.configuration["DBNAME"];
+            var database = mongoClient.GetDatabase(DBName);
+            var collection = database.GetCollection<FileMetadata>("files");
+            var deleteResult = await collection.DeleteOneAsync(p => p.Id == fileId);
+            if (deleteResult.IsAcknowledged)
+            {
+                await SendAsync(deleteResult, 202, cancellation: ct);
+            }
+            else
+            {
+                await SendErrorsAsync();
+            }
+        }
+    }
+}
---
+++ tests/ShareSafe.Tests/Files/DeleteFiles/DeleteFileTests.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using ShareSafe.API.Files.CreateFiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareSafe.Tests.Files.DeleteFiles
+{
+    public class DeleteFileTests : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly CustomWebApplicationFactory customWebApplicationFactory;
+
+        public DeleteFileTests(CustomWebApplicationFactory customWebApplicationFactory)
+        {
+            this.customWebApplicationFactory = customWebApplicationFactory;
+        }
+
+        [Fact]
+        public async Task Should_Get_Error_If_the_File_Id_is_Not_available()
+        {
+            var client = this.customWebApplicationFactory.CreateClient();
+            var response = await client.DeleteAsync("/files/213112");
+            response.Should().Be400BadRequest();
+        }
+
+        [Fact]
+        public async Task Should_Be_Able_to_Get_Accepted_When_trying_to_Delete()
+        {
+            var client = customWebApplicationFactory.CreateClient();
+
+            var createResponse = await client.PostAsJsonAsync<CreateFile>("/files", new CreateFile
+            {
+                Description = "Test File",
+                Name = "Name",
+            });
+
+            var response = await client.DeleteAsync(createResponse.Headers.Location);
+            response.Should().Be202Accepted();
+        }
+    }
+}
