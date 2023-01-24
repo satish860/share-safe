0eab0154f6456e664d9b4e633747d46725a8818b
Satish Venkatakrishnan <satish860@gmail.com>
2023-01-24 18:37:42 +0530

    Download File endpoint done

---
+++ src/ShareSafe.API/Files/DownloadFile/DownloadFileEndpoint.cs
@@ -0,0 +1,48 @@
+﻿using Amazon.S3;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ShareSafe.API.Files.DownloadFile
+{
+    public class DownloadFileEndpoint : EndpointWithoutRequest
+    {
+        private readonly IMongoCollection<FileMetadata> collection;
+        private readonly IAmazonS3 amazonS3;
+        private readonly IConfiguration configuration;
+
+        public DownloadFileEndpoint(IMongoCollection<FileMetadata> collection,
+                                    IAmazonS3 amazonS3,
+                                    IConfiguration configuration)
+        {
+            this.collection = collection;
+            this.amazonS3 = amazonS3;
+            this.configuration = configuration;
+        }
+
+        public override void Configure()
+        {
+            Get("/files/{fileId}/download");
+            AllowAnonymous();
+        }
+
+        public override async Task HandleAsync(CancellationToken ct)
+        {
+            var fileId = Route<ObjectId>("fileId");
+            var filemetadata = await this.collection
+                                         .Find(p => p.Id == fileId)
+                                         .FirstOrDefaultAsync();
+            if (filemetadata == null)
+            {
+                await SendNotFoundAsync();
+            }
+            else
+            {
+                var bucketName = configuration["DOCTL:BucketName"];
+                var response = await amazonS3.GetObjectAsync(bucketName, filemetadata.Name);
+                await SendStreamAsync(response.ResponseStream,
+                        contentType: response.Headers.ContentType,
+                        enableRangeProcessing: true);
+            }
+        }
+    }
+}
